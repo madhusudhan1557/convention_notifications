e1ee389a85e2b1119d4d99ae3218919fed6c413d
Madhusudhan Ghimire <100462733+madhusudhan1557@users.noreply.github.com>
2024-09-23 16:13:01 +0545

    Update NotificationManager.kt

--- android/src/main/kotlin/com/custompackage/convention_notifications/NotificationManager.kt
+++ android/src/main/kotlin/com/custompackage/convention_notifications/NotificationManager.kt
@@ -73,7 +73,6 @@ class NotificationManagerKt(private val context: Context, private val methodChan
             .setContentText(description)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(pendingIntent)
-            .setAllowSystemGeneratedContextualActions()
             .setAutoCancel(true)
 
         with(NotificationManagerCompat.from(context)) {
